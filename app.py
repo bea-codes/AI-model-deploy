cf26d4fd0b8af3f1e0772d2f2199b639642e3b52
dante beatriz <bia906@gmail.com>
2023-12-06 15:27:44 -0300

    Recebimento de inputs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import string
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    values = request.form.get("input")
+    prediction_result = values
+    file = request.files["file_input"]
+    print(file.filename)
+    return render_template("index.html", prediction_result=[prediction_result,file.filename ])
 
 
 if __name__ == "__main__":
