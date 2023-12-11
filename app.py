c2840f852e93dd8ee1599a0db0429e0f5d5d403c
dante beatriz <bia906@gmail.com>
2023-12-11 17:09:34 -0300

    changed model weights

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ transform = transforms.Compose(
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
-alexnet = models.alexnet(pretrained=True)
+alexnet = models.alexnet(weights='IMAGENET1K_V1')
 alexnet.eval()
 
 
